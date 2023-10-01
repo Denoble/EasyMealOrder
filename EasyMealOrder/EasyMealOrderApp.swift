03577facb80413de19578bd65d9f4002ef6e17de
Uche Godfrey <uchegodfrey@Uches-MacBook-Pro-max.local>
2023-10-01 07:59:39 -0400

    first

---
+++ EasyMealOrder/EasyMealOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMealOrderApp.swift
+//  EasyMealOrder
+//
+//  Created by Uche Godfrey on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct EasyMealOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
